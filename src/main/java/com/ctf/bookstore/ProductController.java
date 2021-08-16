a8c5b46b84b497db53d0bc5b8c84c164c505ae6c
pariwat-earth <79245162+pariwat-earth@users.noreply.github.com>
2021-08-16 16:36:21 +0700

    Update ProductController.java

--- src/main/java/com/ctf/bookstore/ProductController.java
+++ src/main/java/com/ctf/bookstore/ProductController.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
 public class ProductController {
 	@Autowired
